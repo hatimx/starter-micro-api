425495e34189a1beac1eaec02614a33e90721809
hatimx <91852766+hatimx@users.noreply.github.com>
2024-01-30 19:19:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) { 
+    res.status(200).send("Welcome to Cyclic Server...");
+});
+
 app.listen(port, function () {
     
 });
