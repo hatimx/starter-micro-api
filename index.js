afff334c4dbb59e75c8c4e6747700b712d5eb8fa
hatimx <91852766+hatimx@users.noreply.github.com>
2024-01-30 19:35:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const port = 3000;
 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+function writeData(data) {
+    await s3.putObject({
+            Body: JSON.stringify({key:"value"}),
+            Bucket: "cyclic-cute-robe-tick-eu-west-2",
+            Key: "some_files/my_file.json",
+        }).promise()
+}
+
+function readData() {
+    let my_file = await s3.getObject({
+            Bucket: "cyclic-cute-robe-tick-eu-west-2",
+            Key: "some_files/my_file.json",
+        }).promise()
+
+console.log(JSON.parse(my_file))
+    return data
+}
+
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
