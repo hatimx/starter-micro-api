e1eb873b9cb32ce6095afe84ca3d4e68704bacf3
AHMED-REM <47160379+AHMED-REM@users.noreply.github.com>
2024-01-30 19:12:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.json()); 
+
+app.listen(port, function () {
+  
+});
