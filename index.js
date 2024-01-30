2f586d1b29faf09eb5a039a8763d0061355ac529
AHMED-REM <47160379+AHMED-REM@users.noreply.github.com>
2024-01-30 19:48:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const port = 3000;
 ////////////////////////////////////////////////////////////////////////////////
 
 function writeData(data){
-    await s3.putObject({
+    s3.putObject({
             Body: JSON.stringify(data),
             Bucket: "cyclic-clean-red-school-uniform-eu-west-2",
             Key: "data/data.json",
-        }).promise()
+        })
 }
 
 function readData(){
-  let data = await s3.getObject({
+  let data = s3.getObject({
             Bucket: "cyclic-clean-red-school-uniform-eu-west-2",
             Key: "data/data.json",
-        }).promise()
+        })
   return JSON.parse(data)
 }
 
