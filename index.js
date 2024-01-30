f4a1d016192515f2933daeeef57a447e62094f83
AHMED-REM <47160379+AHMED-REM@users.noreply.github.com>
2024-01-30 19:41:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3()
+const app = express();
 const port = 3000;
 
-const app = express();
-app.use(bodyParser.json()); 
+////////////////////////////////////////////////////////////////////////////////
 
+function writeData(data){
+    await s3.putObject({
+            Body: JSON.stringify(data),
+            Bucket: "cyclic-clean-red-school-uniform-eu-west-2",
+            Key: "data/data.json",
+        }).promise()
+}
+
+function readData(){
+  let data = await s3.getObject({
+            Bucket: "cyclic-clean-red-school-uniform-eu-west-2",
+            Key: "data/data.json",
+        }).promise()
+  return JSON.parse(data)
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 app.get('/', function (req, res) {
   res.status(200).send("Welcome to API on CYLIC Server");
 });
-
-
 
 
 
