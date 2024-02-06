e56fc439c5102e987a1c205504d24f86220bb250
hatimx <91852766+hatimx@users.noreply.github.com>
2024-02-06 20:13:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(bodyParser.json())
 const port = 3000
 
-let students = JSON.parse("{}")
+let students = JSON.parse("{}") 
 
 ////////////////////////////////////////////////////////////////////////////////
 
