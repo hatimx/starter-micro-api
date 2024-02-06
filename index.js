5d1140df2b8132a6c5e0f2018aec3ef038747ca2
AHMED-REM <47160379+AHMED-REM@users.noreply.github.com>
2024-02-06 01:50:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/students', function (req, res) {
 app.post('/addstudent', function (req, res) {
     let student = req.body
     writeData2(student)
-    res.status(200).send("student added successfully !")
+    res.status(200).send(student)
 })
 
 ////////////////////////////////////////////////////////////////////////////////
