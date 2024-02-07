f30173ef11253c6ae00b44324d1082da3e6c7b25
hatimx <91852766+hatimx@users.noreply.github.com>
2024-02-07 07:52:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 app.use(bodyParser.json())
 const port = 3000
 
-let students = JSON.parse("{}") 
+let students = JSON.parse("{}")
+
+let seances = JSON.parse("{}")
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -28,8 +30,25 @@ app.post('/addstudent', function (req, res) {
     let student = req.body
     keys = Object.keys(student)
     fkey = keys[0]
-    students[fkey] = student[fkey]
+    students[fkey] = student[fkey] 
     res.status(200).send("student saved successfully !")
+})
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+app.get('/seances', function (req, res) {
+    res.status(200).send(seances)
+})
+
+////////////////////////////////////////////////////////////////////////////////
+
+app.post('/addseance', function (req, res) {
+    let seance = req.body
+    keys = Object.keys(seance)
+    fkey = keys[0]
+    seances[fkey] = seance[fkey] 
+    res.status(200).send("seance saved successfully !")
 })
 
 ////////////////////////////////////////////////////////////////////////////////
